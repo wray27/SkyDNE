a04dbd1724474e9dce285e2e267fa8bd86954b22
Aaron Wray <aw16997@bristol.ac.uk>
2022-05-30 19:34:00 +0100

    updates Dockerfile with ssh and test user

--- api/netconf_api/index.py
+++ api/netconf_api/index.py
@@ -2,8 +2,10 @@
 
 from flask import Flask
 
+from .utilities.etwork_interface import NetworkInterface
+
 app = Flask(__name__)
 
 @app.route("/configure_interface")
 def configure_interface():
-  return ''
+    return ''
